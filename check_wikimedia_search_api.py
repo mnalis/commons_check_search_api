f47426fb8e4eee0be6a1d6707812c8b16e2204a0
Matija Nalis <mnalis-git@voyager.hr>
2024-06-02 19:53:28 +0200

    cleaner output

--- check_wikimedia_search_api.py
+++ check_wikimedia_search_api.py
@@ -1,21 +1,24 @@
 import requests
 import csv
+import urllib.parse
 
-def call_api(url, params):
-    response = requests.get(url, params=params)
-    return response.json()
+def call_api(base_url, params):
+    full_url = f"{base_url}?{urllib.parse.urlencode(params)}"
+    response = requests.get(full_url)
+    return response.json(), full_url
 
 def check_match(result, expected_result):
-    return 'yes' if expected_result.lower() in (str(result).lower()) else 'no'
+    return 'yes' if expected_result in str(result) else 'no'
 
-def process_api(url, params, search_term, expected_result):
-    result = call_api(url, params)
+def process_api(api_name, base_url, params, search_term, expected_result):
+    result, full_url = call_api(base_url, params)
     was_match_found = check_match(result, expected_result)
-    return [url, expected_result, search_term, was_match_found]
+    return [api_name, expected_result, search_term, was_match_found, full_url]
 
 def main(search_term, expected_result):
     apis = [
         {
+            "name": "query_apfrom",
             "url": "https://commons.wikimedia.org/w/api.php",
             "params": {
                 "format": "json",
@@ -28,6 +31,7 @@ def main(search_term, expected_result):
             }
         },
         {
+            "name": "Special:UploadWizard",
             "url": "https://commons.wikimedia.org/w/api.php",
             "params": {
                 "action": "opensearch",
@@ -42,12 +46,12 @@ def main(search_term, expected_result):
     
     results = []
     for api in apis:
-        result = process_api(api['url'], api['params'], search_term, expected_result)
+        result = process_api(api['name'], api['url'], api['params'], search_term, expected_result)
         results.append(result)
     
     with open('output.csv', 'w', newline='') as file:
         writer = csv.writer(file, delimiter='|')
-        writer.writerow(["API URL", "Expected Result", "Search Term", "Was Match Found"])
+        writer.writerow(["API", "Expected Result", "Search Term", "Was Match Found", "API URL"])
         writer.writerows(results)
 
 if __name__ == "__main__":
@@ -60,4 +64,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.search_term, args.expected_result)
-
